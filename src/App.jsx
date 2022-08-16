c61115262c121da5a0ce2e41cf645b17ab6d9763
Jamie Ahmed <jamieahmed273@gmail.com>
2022-08-15 20:34:09 -0400

    test for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import './App.css';
-import Navigation from './Components/Navigation';
-import Home from './Components/Home';
-import About from './Components/About';
-import Skill from "./Components/Skills/Skill";
-import Projects from './Components/Projects';
-import Testimonial from './Components/Testimonial';
-import Footer from './Components/Footer';
+// import './App.css';
+// import Navigation from './Components/Navigation';
+// import Home from './Components/Home';
+// import About from './Components/About';
+// import Skill from "./Components/Skills/Skill";
+// import Projects from './Components/Projects';
+// import Testimonial from './Components/Testimonial';
+// import Footer from './Components/Footer';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navigation />
+//       <Home />
+//       <About />
+//       <Skill />
+//       <Projects />
+//       <Testimonial />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+
+const App = () => {
   return (
-    <div className="App">
-      <Navigation />
-      <Home />
-      <About />
-      <Skill />
-      <Projects />
-      <Testimonial />
-      <Footer />
+    <div>
+      <h1>Tesst me jamie</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
