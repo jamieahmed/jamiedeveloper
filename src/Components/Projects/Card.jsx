b9406ae314ef7a596afa1a4be5d797c39e9cda0c
Jamie Ahmed <jamieahmed273@gmail.com>
2022-08-18 23:12:48 -0400

    delete overlay style  from projects's card

---
+++ src/Components/Projects/Card.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const Card = ({ thing, filterState }) => {
+  const [styleStuff, setStyleStuff] = useState({
+    display: "block",
+  });
+
+  useEffect(() => {
+    if (thing.category !== filterState && filterState !== "All") {
+      setStyleStuff({
+        display: "none",
+      });
+    } else {
+      setStyleStuff({
+        display: "block",
+      });
+    }
+  }, [filterState]);
+
+  return (
+    <div className="card" style={styleStuff}>
+      <img className="picture" src={thing.picture} alt="pic" />
+      <p className="title">{thing.title}</p>
+      <p className="status">{thing.status}</p>
+      <p className="languages">{thing.languages}</p>
+      <p className="description">{thing.description}</p>
+      <div>
+        <a href={thing.githubURL}>
+          <button className="go-to-link">Github</button>
+        </a>
+        <a href={thing.deploymentURL}>
+          <button className="go-to-link">Demo</button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
