ed41d65ed41fb231c6d43d1829ec5fddb600c05c
Jamie Ahmed <jamieahmed273@gmail.com>
2022-09-15 23:49:01 -0400

    update testimonal pic

--- src/Components/Testimonial/TestimonialData.js
+++ src/Components/Testimonial/TestimonialData.js
@@ -1,39 +1,39 @@
 const testimonialData = [
     {
-        picture: "../Images/jamie1.jpg",
-        name: "Jamie Ahmed",
+        picture: "https://tse2.mm.bing.net/th?id=OIP.o5hnVgDkhrAIKPAUMAtzcAHaHa&pid=Api&P=0",
+        name: "name of person",
         role: "role of person",
         description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus proin nibh nisl condimentum id. Mattis aliquam faucibus purus in massa. In arcu cursus euismod quis. Egestas fringilla phasellus faucibus scelerisque eleifend.",
         stars: 5,
     },
     {
-        picture: "../Images/jamie1.jpg",
-        name: "nameofperson",
+        picture: "https://tse2.mm.bing.net/th?id=OIP.o5hnVgDkhrAIKPAUMAtzcAHaHa&pid=Api&P=0",
+        name: "name of person",
         role: "role of person",
         description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus proin nibh nisl condimentum id. Mattis aliquam faucibus purus in massa. In arcu cursus euismod quis. Egestas fringilla phasellus faucibus scelerisque eleifend.",
         stars: 5,
     },
     {
-        picture: "../Images/jamie1.jpg",
-        name: "nameofperson",
+        picture: "https://tse2.mm.bing.net/th?id=OIP.o5hnVgDkhrAIKPAUMAtzcAHaHa&pid=Api&P=0",
+        name: "name of person",
         role: "role of person",
         description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus proin nibh nisl condimentum id. Mattis aliquam faucibus purus in massa. In arcu cursus euismod quis. Egestas fringilla phasellus faucibus scelerisque eleifend.",
         stars: 5,
     },
     {
-        picture: "../Images/jamie1.jpg",
-        name: "nameofperson",
+        picture: "https://tse2.mm.bing.net/th?id=OIP.o5hnVgDkhrAIKPAUMAtzcAHaHa&pid=Api&P=0",
+        name: "name of person",
         role: "role of person",
         description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus proin nibh nisl condimentum id. Mattis aliquam faucibus purus in massa. In arcu cursus euismod quis. Egestas fringilla phasellus faucibus scelerisque eleifend.",
         stars: 5,
     },
     {
-        picture: "../Images/jamie1.jpg",
-        name: "nameofperson",
+        picture: "https://tse2.mm.bing.net/th?id=OIP.o5hnVgDkhrAIKPAUMAtzcAHaHa&pid=Api&P=0",
+        name: "name of person",
         role: "role of person",
         description:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus proin nibh nisl condimentum id. Mattis aliquam faucibus purus in massa. In arcu cursus euismod quis. Egestas fringilla phasellus faucibus scelerisque eleifend.",
